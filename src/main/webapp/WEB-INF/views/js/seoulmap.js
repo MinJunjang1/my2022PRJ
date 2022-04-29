91846725245c015a393891d530c9b6983936e08d
jmj <ccab147)@gmail.com>
2022-04-29 15:18:48 +0900

    Changes

---
+++ src/main/webapp/WEB-INF/views/js/seoulmap.js
@@ -0,0 +1,36 @@
+//서울
+function setSeoul() {
+    // 이동할 위도 경도 위치를 생성합니다
+    var moveLatLon = new kakao.maps.LatLng(37.561, 126.986);
+    map.setCenter(moveLatLon);
+    var level = map.getLevel();
+    // 지도를 1레벨 내립니다 (지도가 확대됩니다)
+    map.setLevel(7);
+    // 지도 레벨을 표시합니다
+    displayLevel();
+    // 지도 중심을 이동 시킵니다
+}
+//종로구
+function setJongnogu() {
+    // 이동할 위도 경도 위치를 생성합니다
+    var moveLatLon = new kakao.maps.LatLng(37.5703, 126.9816);
+    map.setCenter(moveLatLon);
+    var level = map.getLevel();
+    // 지도를 1레벨 내립니다 (지도가 확대됩니다)
+    map.setLevel(5);
+    // 지도 레벨을 표시합니다
+    displayLevel();
+    // 지도 중심을 이동 시킵니다
+}
+//중구
+function setJunggu() {
+    // 이동할 위도 경도 위치를 생성합니다
+    var moveLatLon = new kakao.maps.LatLng(37.5700, 126.979);
+    map.setCenter(moveLatLon);
+    var level = map.getLevel();
+    // 지도를 1레벨 내립니다 (지도가 확대됩니다)
+    map.setLevel(5);
+    // 지도 레벨을 표시합니다
+    displayLevel();
+    // 지도 중심을 이동 시킵니다
+}
