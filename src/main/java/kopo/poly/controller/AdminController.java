5f247ceecfdaab6328e9bc9aee74d28db3642c5b
jmj <ccab1470@gmail.com>
2022-05-12 08:42:03 +0900

    commit

--- src/main/java/kopo/poly/controller/AdminController.java
+++ src/main/java/kopo/poly/controller/AdminController.java
@@ -1,23 +1,18 @@
 package kopo.poly.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.logging.Logger;
-
 @Slf4j
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(AdminController.class);
 
     @RequestMapping(value = "main", method = RequestMethod.GET)
     public void adminmainpage() throws Exception{
-        logger.info("관리자페이지 이동");
     }
     @GetMapping(value = "/main")
     public String mainpage() throws Exception {
