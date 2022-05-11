162afcfefdc1233b986a1fba3f7e1847dcd9903a
jmj <ccab1470@gmail.com>
2022-05-12 08:43:24 +0900

    commit

--- src/main/java/kopo/poly/controller/AdminController.java
+++ src/main/java/kopo/poly/controller/AdminController.java
@@ -4,16 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Slf4j
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-    @RequestMapping(value = "main", method = RequestMethod.GET)
-    public void adminmainpage() throws Exception{
-    }
     @GetMapping(value = "/main")
     public String mainpage() throws Exception {
         return "/admin/main";
